5c6f725f5ddee5ad2eda25ad32d3cd38074a4bdf
sebramirez44 <sebramirezcordero@gmail.com>
2023-09-11 21:00:53 -0600

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+    let count = 0;
+    console.log(count);
+</script>  
+<h1>Counter: {count}</h1>
+<button on:click={() => count+=1}>Increase</button>
+<button on:click={() => count-=1}>Decrease</button>
