4d7b435c2182db4f1070fedd0eae38c505c0cf38
sebramirez44 <sebramirezcordero@gmail.com>
2023-09-11 21:20:33 -0600

    .

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,3 +5,4 @@
 <h1>Counter: {count}</h1>
 <button on:click={() => count+=1}>Increase</button>
 <button on:click={() => count-=1}>Decrease</button>
+<button on:click={() => count*=2}>Double</button>
